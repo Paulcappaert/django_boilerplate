60f932c28b1f8289b061219a56932070ce8d6525
Paul Cappaert <paul.cappaert@gmail.com>
2020-02-22 18:05:45 -0500

    changed consumer to async to better use database connections

--- game/consumers.py
+++ game/consumers.py
@@ -1,43 +1,38 @@
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 from channels.auth import get_user
 from .models import Game
+from channels.db import database_sync_to_async
 
-class GameConsumer(WebsocketConsumer):
+class GameConsumer(AsyncWebsocketConsumer):
 
-    def connect(self):
+    async def connect(self):
         self.game_code = self.scope['url_route']['kwargs']['game']
-        if Game.objects.filter(code=self.game_code).exists():      
+        if await self.game_exists(self.game_code):      
             self.group_name = f'chess_{self.game_code}'
             
-            # Join room group
-            async_to_sync(self.channel_layer.group_add)(
+            await self.channel_layer.group_add(
                 self.group_name,
-                self.channel_name
+                self.channel_name,
             )
 
             self.accept()
 
-    def disconnect(self, close_code):
-        # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
             self.group_name,
             self.channel_name
         )
 
     # Receive message from WebSocket
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         source = text_data_json['source']
         target = text_data_json['target']
-        
-        game = Game.objects.get(code=self.game_code)
-        user = self.scope['user']
 
-        if game.can_move(user):
-            game.make_move(source, target)
-            async_to_sync(self.channel_layer.group_send)(
+        if await self.make_move(source, target):
+            await self.channel_layer.group_send(
                 self.group_name,
                 {
                     'type': 'game_move',
@@ -49,8 +44,20 @@ class GameConsumer(WebsocketConsumer):
             )
 
     # Receive message from room group
-    def game_move(self, event):
+    async def game_move(self, event):
         move = event['move']
+        await self.send(text_data=move)
 
-        # Send message to WebSocket
-        self.send(text_data=move)
+    @database_sync_to_async
+    def game_exists(self):
+        return Game.objects.filter(code=self.game_code).exists()
+
+    @database_sync_to_async
+    def make_move(self, source, target):
+        game = Game.objects.get(code=self.game_code)
+        user = self.scope['user']
+        if game.can_move(user):
+            game.make_move(source, target)
+            return True
+        else:
+            return False
