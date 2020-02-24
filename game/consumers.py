831a2d2210b757b253698eecca1d030bf8b4c85a
Paul Cappaert <paul.cappaert@gmail.com>
2020-02-24 12:50:03 -0500

    removed old code

--- game/consumers.py
+++ game/consumers.py
@@ -1,7 +1,5 @@
-from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
-from .models import Game
 
 class GameConsumer(AsyncWebsocketConsumer):
 
